830ea5a21ac09a3656569f0f4a8b49a91033498c
Mehedi Mostafa Hafiz <mehedimostafabisd@gmail.com>
2026-01-02 23:02:44 +0000

    Revamp number guessing game logic and range
    Expanded the guessing range from 1-10 to 1-100 and implemented a loop to allow multiple guesses with feedback on each attempt. The game now informs the player if their guess is too high or too low and continues until the correct number is guessed.

--- My_solution/01_number_guess.py
+++ My_solution/01_number_guess.py
@@ -1,9 +1,19 @@
+# The player will try to guess a randomly generated number between 1 and 100.
+# The program will provide feedback on whether the guess is too low, too high, or correct.
+# The game continues until the player guesses the correct number.
 import random
 
-secret_number = random.randint(1, 10)
-guess = int(input("Guess a number between 1 and 10: "))
-
-if guess == secret_number:
-    print("You got it!")
-else:
-    print(f"Wrong! The number was {secret_number}")
+number = random.randint(1, 100)
+attempt = 1
+print("Welcome to number guessing game!!")
+print(number)
+guess = input("Guess a number between (1-100):")
+while guess != number:
+    guess = input("Guess a number between (1-100)")
+    if guess != number:
+        if guess < number:
+            print("Incorrect guess, Low")
+        elif guess > number:
+            print("Incorrect guess, High")
+    else:
+        print(f"You win the number was {number}")
